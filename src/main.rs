d9da89dc9da1e95caff1093c1e9a8d82fcc4f0ec
Morgan Bentell <morgan.bentell@gmail.com>
2022-12-20 22:37:40 +0100

    toggling led with button

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate rust_gpiozero;
+
+use std::time::Duration;
+use rust_gpiozero::*;
+
 fn main() {
-    println!("Hello, led!");
+    // Tell the Pi which GPIO pin you are using
+    let mut led = LED::new(17);
+
+    // Create a button which is attached to Pin 22
+    let mut button = Button::new(22);
+
+    loop{
+        button.wait_for_press(None);
+        println!("Press!");
+        led.toggle();
+        std::thread::sleep(Duration::from_secs(1));
+
+    }
 }
